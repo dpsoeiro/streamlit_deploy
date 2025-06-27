02ee4e08eaaff1950781f2860a68711a0e516e98
Daniel Soeiro <dpsoeiro@hotmail.com>
2025-06-26 22:49:53 -0300

    pasta de plugins + atualização requirements

--- helper/Helper.py
+++ helper/Helper.py
@@ -1,5 +1,35 @@
 import pandas as pd
+import yaml
 
 
+
+
+
+class Helper:
+
+
+
+    def __init__(self):
+        self.config_path =  "config\\paths.yaml"
+
+    def open_yaml(self):
+        arquivo_yaml = self.config_path
+        with open(arquivo_yaml, 'r') as arquivo:
+            config = yaml.safe_load(arquivo)
+
+        self.config = config
+    
+    def load_main_file(self):
+
+        config = self.config
+
+        players_path = config['players_path']
+
+
+        df_data = pd.read_csv(players_path, index_col=0)
+        df_data['Playing Time Min'] = int(df_data['Playing Time Min'])
+        df_data = df_data[df_data['Playing Time Min'] >= 0]
+        
+        return df_data
 
 
--- main_page.py
+++ main_page.py
@@ -2,8 +2,15 @@ import streamlit as st
 import webbrowser
 import pandas as pd
 from datetime import datetime 
+from helper import Helper
 
-def load_data
+
 
 
 if "data" not in st.session_state:
+
+    st.session_state["data"] = Helper.load_main_file
+
+
+st.markdown("# SISTEMA FUTCODE DE MONITORAMENTO E RECOMENDAÇÃO DE ATLETAS! ⚽️")
+st.sidebar.markdown("Desenvolvido por Daniel Pessoa Soeiro")
