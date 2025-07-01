fabcd19eb4cf7d24a62e644524f225039c72a55a
Daniel Soeiro <dpsoeiro@hotmail.com>
2025-06-30 23:04:57 -0300

    arquivos de imagem e alterações na pagina inicial

--- helper/Helper.py
+++ helper/Helper.py
@@ -11,13 +11,16 @@ class Helper:
 
     def __init__(self):
         self.config_path =  "config\\paths.yaml"
-
+    
+    
     def open_yaml(self):
         arquivo_yaml = self.config_path
         with open(arquivo_yaml, 'r') as arquivo:
             config = yaml.safe_load(arquivo)
 
         self.config = config
+
+        return config
     
     def load_main_file(self):
 
@@ -27,7 +30,7 @@ class Helper:
 
 
         df_data = pd.read_csv(players_path, index_col=0)
-        df_data['Playing Time Min'] = int(df_data['Playing Time Min'])
+        df_data['Playing Time Min'] = df_data['Playing Time Min'].astype(int)
         df_data = df_data[df_data['Playing Time Min'] >= 0]
         
         return df_data
--- main_page.py
+++ main_page.py
@@ -4,13 +4,29 @@ import pandas as pd
 from datetime import datetime 
 from helper import Helper
 
+st.set_page_config(
+    page_title="Principal",
+    page_icon="🏃🏼",
+    layout="wide"
+)
 
+
+helper = Helper()
+
+### variaveis
+
+arquivo_config = helper.open_yaml()
+logo_futcode = arquivo_config['logo_principal']
 
 
 if "data" not in st.session_state:
 
-    st.session_state["data"] = Helper.load_main_file
+    st.session_state["data"] = helper.load_main_file()
 
 
-st.markdown("# SISTEMA FUTCODE DE MONITORAMENTO E RECOMENDAÇÃO DE ATLETAS! ⚽️")
+st.header("SISTEMA :blue[FUTCODE] DE MONITORAMENTO E RECOMENDAÇÃO DE ATLETAS! ⚽️", width = "stretch" )
+
+
+# Inserir imagem na sidebar
+st.sidebar.image(logo_futcode, use_container_width =True)
 st.sidebar.markdown("Desenvolvido por Daniel Pessoa Soeiro")
